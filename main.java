e69a3b5185655ab843ad43739665fbd93be00fa7
hafsa-ziaa <f223717@cfd.nu.edu.pk>
2024-10-05 16:19:06 +0500

    Added Main class to test libraay functions

---
+++ main.java
@@ -0,0 +1,18 @@
+package git;
+
+public class main {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			Library myLibrary = new Library();
+			myLibrary.addBook(&quot;Harry Potter&quot;);
+			myLibrary.addBook(&quot;Lord of the Rings&quot;);
+			myLibrary.listBooks();
+			}
+			
+	}
+
+}
